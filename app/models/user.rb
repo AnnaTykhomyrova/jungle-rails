10e68b2cd98e8a08300b0873c24842fba353edb5
AnnaTykhomyrova <tykhomyrovaanna@gmail.com>
2019-07-15 17:59:54 +0000

    New authentication (class) method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ActiveRecord::Base
     validates :email, presence: true, :uniqueness => { :case_sensitive => false }
     validates :password, presence: true, length: { minimum: 5}
     validates :password_confirmation, presence: true
+
+    def self.authenticate_with_credentials(email, password)
+        # byebug
+        user = User.find_by_email(email.strip.downcase)
+        if user && user.authenticate(password)
+          return user
+        else
+          return false
+        end
+      end
 end
